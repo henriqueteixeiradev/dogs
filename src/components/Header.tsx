20f39c929432d36e752973d277ee0eb0ef826e44
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-05-26 11:35:28 -0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { dog } from "../svg/dog";
 
 export function Header() {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          {dog}
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 }
